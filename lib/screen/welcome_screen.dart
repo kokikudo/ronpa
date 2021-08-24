b6a91f1dceaa024c15e6e8b97400a63a9e3a1d3a
kokikudo <massanmesu@icloud.com>
2021-08-24 15:18:03 +0900

    ホーム画面のレイアウト作成。TextFieldの項目名をラベルからhelperに変更。

--- lib/screen/welcome_screen.dart
+++ lib/screen/welcome_screen.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:ronpa/components/bobble_button.dart';
+import 'package:ronpa/components/login_input_field.dart';
+import 'package:ronpa/constraints.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   static const String id = '/home';
 
   @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool isUserNameError = false;
+  bool isPasswordError = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    TextTheme _textTheme = Theme.of(context).textTheme;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/anotherBackgroundImage2.jpg'),
+          fit: BoxFit.fitHeight,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/appIcon.png'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              Text('Ronpa!', style: _textTheme.headline2),
+            ],
+          ),
+          SizedBox(height: 30),
+          isUserNameError
+              ? LoginInputField(title: 'ユーザー名', errorMessage: 'ユーザー名が一致しません')
+              : LoginInputField(title: 'ユーザー名', errorMessage: null),
+          SizedBox(height: 20),
+          isPasswordError
+              ? LoginInputField(title: 'パスワード', errorMessage: 'パスワードが一致しません')
+              : LoginInputField(title: 'パスワード', errorMessage: null),
+          SizedBox(height: 40),
+          Bobble(bobbleText: 'ログイン', backgroundColor: kRed, onPressed: () {}),
+          SizedBox(height: 20),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              '新規登録',
+              style: _textTheme.button,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
